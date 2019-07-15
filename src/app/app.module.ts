c98664f680d94074c5cbc2903159831951cb4393
Maximiliano Echevarria <maximiliano.echevarria@hotmail.com>
2019-07-15 15:07:59 -0300

    Rutas del proyecto creadas y configuradas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
+
+//Rutas
+import { APP_ROUTING } from './app.routes';
+
+//Componentes
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { ArtistsComponent } from './components/artists/artists.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+
 
 @NgModule({
   declarations: [
@@ -16,7 +25,11 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    APP_ROUTING
+
   ],
   providers: [],
   bootstrap: [AppComponent]
