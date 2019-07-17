6e13c23cd381b8df37962c9beb7fab88b04ae9b4
Maximiliano Echevarria <maximiliano.echevarria@hotmail.com>
2019-07-17 10:00:13 -0300

    Centralizamos las peticiones para un mejor manejo de las mismas en el tiempo

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -9,28 +9,25 @@ export class SpotifyService {
 
   constructor( private http: HttpClient ) { }
 
-  getNewReleases(){
-      const headers = new HttpHeaders({
-        'Authorization': 'Bearer BQB7oPFuFWjeyR3-Dpw09y2U4JDjLRgR-a9YqCYlOpWOheYkMxQOInx8HuE_yaI2DwSQn9SQGwruDhZDnBE'
-      })
+  getQuery( query:string ){
+      const url = `https://api.spotify.com/v1/${query}`;
 
-      return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers })
-                      .pipe( map( ( data:any ) => {
-                            return data.albums.items
-                      }));
-  }  
+      const headers = new HttpHeaders({
+        'Authorization': 'Bearer BQCSR-qr_CAJBAt0Pirri8Qmdb9O9bjcWsQ7LAQ0MuISZ9dWF4wMKrP270cE7It-X4sFWQQkJaijtnsamjQ'
+      });
+
+      return this.http.get( url, { headers } );
+
+  };
+
+  getNewReleases(){
+      return this.getQuery('browse/new-releases?limit=20')
+                 .pipe( map( ( data:any ) => data.albums.items ));
+  };
   
   getArtis( artist:string ){
-    
-    const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQB7oPFuFWjeyR3-Dpw09y2U4JDjLRgR-a9YqCYlOpWOheYkMxQOInx8HuE_yaI2DwSQn9SQGwruDhZDnBE'
-    })
-    
-    return this.http.get(`https://api.spotify.com/v1/search?q=${ artist }&type=artist&limit=20`, { headers })
-                    .pipe( map( data => {
-                          return data['artists'].items
-                    }))
-  
-  }
+      return this.getQuery(`search?q=${ artist }&type=artist&limit=20`)
+                 .pipe( map( data => data['artists'].items ));             
+  };
 
 }
